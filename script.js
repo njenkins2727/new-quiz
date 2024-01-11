042e9f1c5a2a2eee758844f8cfdeaf7206a1cafa
njenkins2727 <njenkins2727@gmail.com>
2024-01-11 18:39:26 +0800

    added little functionality to quiz, work in progress

--- script.js
+++ script.js
@@ -5,12 +5,107 @@ const intro = document.getElementById('intro');
 const btnTwo = document.getElementById('btn-2');
 
 const ready = document.getElementById('confirm');
+const btnThree = document.getElementById('btn-3');
 
 const quizQuestions = document.getElementById('quiz-q');
 
-//when i click btnOne, i am taken to intro 
+const quizTitle = document.getElementById('q-title');
+const quizChoice = document.getElementById('q-text');
 
-// when i click btntwo, i am taken to question 
+const question = [
+    {
+        title: "My favourite food",
+        answers: [
+          { text: "Curried beans", correct: false },
+          { text: "Lamb saganaki", correct: false },
+          { text: "Coconut curry", correct: true},
+          { text: "Burrito bowl", correct: false }
+        ]
+      },
+      {
+          title: "My favourite food",
+            answers: [
+              { text: "Curried beans", correct: false },
+              { text: "Lamb saganaki", correct: false },
+              { text: "Coconut curry", correct: false},
+              { text: "Burrito bowl", correct: false }
+            ]
+      },
+      {
+        title: "My favourite food",
+          answers: [
+            { text: "Curried beans", correct: false },
+            { text: "Lamb saganaki", correct: false },
+            { text: "Coconut curry", correct: false},
+            { text: "Burrito bowl", correct: false }
+          ]
+    },
+    {
+        title: "My favourite food",
+          answers: [
+            { text: "Curried beans", correct: false },
+            { text: "Lamb saganaki", correct: false },
+            { text: "Coconut curry", correct: false},
+            { text: "Burrito bowl", correct: false }
+          ]
+    },
+    {
+        title: "My favourite food",
+          answers: [
+            { text: "Curried beans", correct: false },
+            { text: "Lamb saganaki", correct: false },
+            { text: "Coconut curry", correct: false},
+            { text: "Burrito bowl", correct: false }
+          ]
+    },
+    {
+        title: "My favourite food",
+          answers: [
+            { text: "Curried beans", correct: false },
+            { text: "Lamb saganaki", correct: false },
+            { text: "Coconut curry", correct: false},
+            { text: "Burrito bowl", correct: false }
+          ]
+    },
+    {
+        title: "My favourite food",
+          answers: [
+            { text: "Curried beans", correct: false },
+            { text: "Lamb saganaki", correct: false },
+            { text: "Coconut curry", correct: false},
+            { text: "Burrito bowl", correct: false }
+          ]
+    },
+    {
+        title: "My favourite food",
+          answers: [
+            { text: "Curried beans", correct: false },
+            { text: "Lamb saganaki", correct: false },
+            { text: "Coconut curry", correct: false},
+            { text: "Burrito bowl", correct: false }
+          ]
+    },
+    {
+        title: "My favourite food",
+          answers: [
+            { text: "Curried beans", correct: false },
+            { text: "Lamb saganaki", correct: false },
+            { text: "Coconut curry", correct: false},
+            { text: "Burrito bowl", correct: false }
+          ]
+    },
+    {
+        title: "My favourite food",
+          answers: [
+            { text: "Curried beans", correct: false },
+            { text: "Lamb saganaki", correct: false },
+            { text: "Coconut curry", correct: false},
+            { text: "Burrito bowl", correct: false }
+          ]
+    },
+
+     
+]
 
 btnOne.addEventListener('click', function(){
     landingPg.classList.add("hide");
@@ -22,4 +117,70 @@ btnTwo.addEventListener('click', function(){
     ready.classList.remove('hide');
 })
 
+btnThree.addEventListener('click', function(){
+    ready.classList.add('hide');
+    quizQuestions.classList.remove('hide');
+    displayQuestions(0);
+});
 
+function displayQuestions(questionIndex){
+
+    const questions = question[questionIndex]
+
+    quizTitle.textContent = questions.title;
+
+    quizChoice.textContent = "";
+
+    const quizText = questions.answers
+
+    for (let i = 0; i < quizText.length; i++) {
+        const choices = quizText[i];
+
+        const li = document.createElement('li');
+
+            const button = document.createElement('button');
+    
+            button.setAttribute('class', 'centred btn-q');
+    
+            button.textContent = choices.text;
+    
+            li.appendChild(button);
+    
+            quizChoice.appendChild(li);
+
+        button.addEventListener('click', function(){
+
+            if (choices.correct === false) {
+                return tryAgain();
+            }else {
+                return renderNextQuestion();
+            }
+        })
+           
+    }
+                
+      
+}
+
+// function tryAgain(){
+//     quizQuestions.classList.add('hide');
+//     const div = document.createElement('div');
+//     div.classList.add('try-again-div');
+    
+//     const h1 = document.createElement('h1');
+//     const retryButton = document.createElement('button');
+    
+//     h1.textContent = 'YOU LOSE'
+//     retryButton.textContent = 'Retry'
+    
+//     retryButton.addEventListener('click', function(){
+        
+//         const ready = getElementById('confirm');
+//         div.classList.add('hide');
+//         ready.classList.remove('hide');
+
+//     })
+    
+//     div.appendChild(h1, retryButton);
+
+// }
